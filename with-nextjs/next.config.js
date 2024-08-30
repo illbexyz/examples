16e04a2ccc1fab4d65ce91b7a6ef69409489c665
Alberto Nicoletti <illbexyz@users.noreply.github.com>
2024-08-30 23:44:51 +0200

    fix: ssr styles are now sent Fixes a bug where the styles created by StyleSheet.create() were not sent in the HTML page. Solved by adding react-native-web to the transpiled packages.

--- with-nextjs/next.config.js
+++ with-nextjs/next.config.js
@@ -2,16 +2,17 @@ const { withExpo } = require("@expo/next-adapter");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withExpo({
-  reactStrictMode: true,
-  swcMinify: true,
-  transpilePackages: [
-    "react-native",
-    "expo",
-    // Add more React Native / Expo packages here...
-  ],
-  experimental: {
-    forceSwcTransforms: true,
-  },
+	reactStrictMode: true,
+	swcMinify: true,
+	transpilePackages: [
+		"react-native",
+		"react-native-web",
+		"expo",
+		// Add more React Native / Expo packages here...
+	],
+	experimental: {
+		forceSwcTransforms: true,
+	},
 });
 
 module.exports = nextConfig;
