6abbf1da409e860d18e0dea61b844f731f140579
Alberto Nicoletti <illbexyz@users.noreply.github.com>
2024-08-30 23:40:50 +0200

    feat: add background color

--- with-nextjs/pages/index.js
+++ with-nextjs/pages/index.js
@@ -2,20 +2,21 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Welcome to Expo + Next.js 👋</Text>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text style={styles.text}>Welcome to Expo + Next.js 👋</Text>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  text: {
-    fontSize: 16,
-  },
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "rgb(255 92 202)",
+	},
+	text: {
+		fontSize: 16,
+	},
 });
